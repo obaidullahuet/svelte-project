a828a1bbb4f19602cd8d6c218c3395383713c27d
obaidullahuet <obaidullahset@gmail.com>
2025-07-01 04:43:37 -0700

    reomove vercle files

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr2.js";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class=""> <main class="">${slots.default ? slots.default({}) : ``}</main></div>`;
-});
-export {
-  Layout as default
-};
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.nwMKuLd9.js","_app/immutable/chunks/C_NSvUt5.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/BX655cEg.js"];
-export const stylesheets = [];
-export const fonts = [];
